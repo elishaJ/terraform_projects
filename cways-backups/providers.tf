25c49b61278cfb4a1da3c7e975e095b3fd65e535
Elisha Jamil <elisha.jamil@gmail.com>
2023-11-30 01:48:26 +0500

    Update providers.tf
    Add AWS provider

--- cways-backups/providers.tf
+++ cways-backups/providers.tf
@@ -4,8 +4,12 @@ terraform {
       source  = "hashicorp/google"
       version = "5.7.0"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.28.0"
+    }
   }
-
+  
   backend "s3" {
     bucket = "tf-state-bk-007"
     key    = "terraform_projects/cwaysbackups/terraform.tfstate"
